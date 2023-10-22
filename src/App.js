e8d8bf7702d2494c8f3bd91f1ed14bf94b04f1eb
LeTruongLam <104148284+LeTruongLam@users.noreply.github.com>
2023-10-22 20:43:33 +0700

    UI:login

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import "./App.css";
+import TheHeader from "./component/TheHeader/TheHeader";
+import TheLogin from "./pages/Login/TheLogin";
+import TheRegister from "./pages/Register/TheRegister"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Route,
+  Outlet,
+} from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <TheHeader />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/login",
+        element: <TheLogin />,
+      },
+      {
+        path: "/register",
+        element: <TheRegister />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <div>This is Register!</div>,
+  },
+  {
+    path: "/login",
+    element: <div>This is Login!</div>,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
