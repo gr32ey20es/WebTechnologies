11235c756967456e14dab29e19d6799bf4b05a7e
RiverShen31 <tienlam3105@gmail.com>
2023-11-12 20:11:08 +0700

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import {RouterProvider, router} from './login/RouterProvider';
 // import Test from './test/Test';
 import './App.css';
+import Homepage from './homepage/Homepage.js';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <BrowserRouter>
+      <Routes>
+        <Route path='/homepage' element={<Homepage />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
