d53788db441bbf50cd5f40c82b774852e68e3a93
omgnt199 <omgnt199@gmail.com>
2023-11-26 10:13:50 +0700

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,14 @@
-import TheHeader from "./login/component/TheHeader/TheHeader";
-import TheLogin from "./login/pages/Login/TheLogin";
-import TheRegister from "./login/pages/Register/TheRegister"
-import Dashboard from "./login/pages/Dashboard/Dashboard.jsx";
-import Homepage from './homepage/Homepage.js';
 import './App.css';
+import ContentHeader from './components/Content/ContentHeader';
+import Header from './components/Header/Header';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  // Route,
-  Outlet,
-} from "react-router-dom";
-import ContentHeader from "./course-online/Content/ContentHeader.jsx";
-
-const Layout = () => {
-  return (
-    <>
-      <TheHeader />
-      <Outlet />
-    </>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/login",
-        element: <TheLogin />,
-      },
-      {
-        path: "/register",
-        element: <TheRegister />,
-      },
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
-      },
-      {
-        path: "/homepage",
-        element: <Homepage />,
-      },
-    ],
-  },
-  {
-    path: "/register",
-    element: <div>This is Register!</div>,
-  },
-  {
-    path: "/login",
-    element: <div>This is Login!</div>,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "/homepage",
-    element: <Homepage />,
-  },
-]);
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={router} />
-      <ContentHeader></ContentHeader>
+      <header className="App-header">
+        <Header></Header>
+        <ContentHeader></ContentHeader>
+      </header>
     </div>
   );
 }
